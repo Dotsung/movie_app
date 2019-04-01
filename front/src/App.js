9d2ab41b7c23bde4e4b367ab64a614689116aee4
Dotsung <andy395023@gmail.com>
2019-04-01 23:20:47 +0900

    영화 설명과 장르등 연결

--- front/src/App.js
+++ front/src/App.js
@@ -1,44 +1,44 @@
-import React, { Component } from 'react';
+// @flow
+import React from 'react';
 import './App.css';
 import Movie from './Movie';
 
 
-class App extends Component {
+class App extends React.Component {
 
-  state = {
-  }
+  state = {}
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: '캡틴마블',
-            poster: 'http://image2.megabox.co.kr/mop/poster/2019/FF/35FB82-03E7-4407-95A2-49C9E59B7365.large.jpg'
-          },
-          {
-            title: '부산행',
-            poster: 'https://t1.daumcdn.net/movie/fe9da43b455db93228b5bfa74dacc78107f1eb40'
-          },
-          {
-            title: '마녀',
-            poster: 'http://img.cgv.co.kr/Movie/Thumbnail/Poster/000080/80789/80789_1000.jpg'
-          },
-          {
-            title: '돈',
-            poster: 'https://i.ytimg.com/vi/5cmbydhduHk/maxresdefault.jpg'
-          }
-        ]
-      })
-    }, 5000);
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index)=> {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map((movie)=> {
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        genres={movie.genres}
+        synopsis={movie.synopsis} 
+        key={movie.id}
+       />
     });
     return movies;
   }
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(res => res.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
 
   render() {
     return (
--- front/src/Movie.js
+++ front/src/Movie.js
@@ -1,31 +1,54 @@
 // @flow
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types';
 import './Movie.css'
 
-type props = {
-  title: string,
-  poster: string
-}
-
-class Movie extends React.Component {
-  
-  render() {
-    return (
-        <div>
-            <h2>{this.props.title}</h2>
-            {MoviePoster(this.props.poster)}
+const Movie = ({title, poster, genres, synopsis}) => {
+  return (
+      <div className="Movie">
+        <div className="Movie__Columns">
+          <MoviePoster poster={poster}/>
         </div>
-    );
-  }
+        <div className="Movie__Columns">
+          <h1>{title}</h1>
+          <div className="Movie__Genres">
+            {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}
+          </div>
+          <p className="Movie__Synopsis">
+            {synopsis}
+          </p>
+        </div>
+      </div>
+  );
 }
 
-
-var MoviePoster = (poster) => {
+const MoviePoster = ({poster}) => {
   return (
     <div>
         <img src={poster}></img>
     </div>
   );
+}
+
+const MovieGenre = ({genre}) => {
+  return (
+    <span className="Movie_Genre">{genre}</span>
+  )
+}
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+  genres: PropTypes.array.isRequired
 }
 
 export default Movie;
